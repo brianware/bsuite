d94b33ffdc6d08c32f8052a356ad130b44b9c357
brianware <brianyukc@yahoo.com>
2018-11-03 11:42:53 -0400

    My first commit

--- index.php
+++ index.php
@@ -1,141 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>CSS Template</title>
+<title>Home</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-* {
-    box-sizing: border-box;
-}
+<link rel="stylesheet" href="style.css">
 
-body {
-    margin: 0;
-    font-family: Arial, Helvetica, sans-serif;
-}
-
-/* Style the top navigation bar */
-.topnav {
-    overflow-x: hidden;
-    background-color: #333;
-    position: fixed;
-    top: 0;
-    height: 45px;
-    width: 100%;
-    z-index: 1;
-}
-
-/* Style the topnav links */
-.topnav a {
-    float: left;
-    display: block;
-    color: #f2f2f2;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-}
-
-/* Change color on hover */
-.topnav a:hover {
-    background-color: #ddd;
-    color: black;
-}
-
-/* Style the side navigation */
-.sidenav {
-    height: 100%;
-    width: 200px;
-    position: fixed;
-    z-index: 1;
-    top: 45px;
-    left: 0;
-    background-color: #111;
-    overflow-x: hidden;
-}
-
-/* Side navigation links */
-.sidenav a {
-    color: white;
-    padding: 14px 16px;
-    text-decoration: none;
-    display: block;
-}
-
-/* Change color on hover */
-.sidenav a:hover {
-    background-color: #ddd;
-    color: black;
-}
-
-/* Style the content */
-.content {
-    margin-left: 200px;
-    padding-left: 20px;
-}
-
-/* Style the footer */
-.footer {
-    background-color: #f1f1f1;
-    padding: 10px;
-    position: fixed;
-    bottom: 0;
-    left: 200px;
-    height: 45px;
-    width: 100%;
-}
+<style type="text/css">
+    .fieldset-auto-width {
+         display: inline-block;
+    }
 </style>
-</head>
+</head> 
+
 <body>
-
 <div class="topnav">
-  <a href="#">Link</a>
-  <a href="#">Link</a>
-  <a href="#">Link</a>
-</div>
-
-<div class="sidenav">
-  <a href="#">Link</a>
-  <a href="#">Link</a>
-  <a href="#">Link</a>
+<p style="color:white;padding-left:10px;">B Suite 2018</p>
 </div>
 
 <div class="content">
-  <h2>CSS Template</h2>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-  <p>A full-height, fixed sidenav and content.</p>
-</div>
+<h1>Welcome to B Suite</h1>
+<form id='login' action='login.php' method='post' accept-charset='UTF-8'>
+	<fieldset class="fieldset-auto-width">
+	<legend>Login</legend>
+	<input type='hidden' name='submitted' id='submitted' value='1'/>
+	<label for='username' >User Name:</label>
+	<input type='text' name='username' id='username'  maxlength="50" />
+	<label for='password' >Password:</label>
+	<input type='password' name='password' id='password' maxlength="50" />
+	<input type='submit' name='Submit' value='Submit' />
+	</fieldset>
+	</form>
 
 <div class="footer">
-  <p>Footer</p>
+  <p>Copyright © 2018 by Brianware</p>
 </div>
-
 </body>
-</html>
+  </html>
---
+++ signon.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Sign On</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="style.css">
+</head> 
+
+<div class="content">
+
+<h2>CSS Template</h2>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  </div>
+
+  </html>
