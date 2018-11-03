832666a765e071cdebb901fe04d6b9d659c06d77
brianware <brianyukc@yahoo.com>
2018-11-03 07:09:28 -0400

    My first commit

--- index.php
+++ index.php
@@ -1,19 +1,141 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<title>CSS Template</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+    box-sizing: border-box;
+}
+
+body {
+    margin: 0;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+/* Style the top navigation bar */
+.topnav {
+    overflow-x: hidden;
+    background-color: #333;
+    position: fixed;
+    top: 0;
+    height: 45px;
+    width: 100%;
+    z-index: 1;
+}
+
+/* Style the topnav links */
+.topnav a {
+    float: left;
+    display: block;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+/* Change color on hover */
+.topnav a:hover {
+    background-color: #ddd;
+    color: black;
+}
+
+/* Style the side navigation */
+.sidenav {
+    height: 100%;
+    width: 200px;
+    position: fixed;
+    z-index: 1;
+    top: 45px;
+    left: 0;
+    background-color: #111;
+    overflow-x: hidden;
+}
+
+/* Side navigation links */
+.sidenav a {
+    color: white;
+    padding: 14px 16px;
+    text-decoration: none;
+    display: block;
+}
+
+/* Change color on hover */
+.sidenav a:hover {
+    background-color: #ddd;
+    color: black;
+}
+
+/* Style the content */
+.content {
+    margin-left: 200px;
+    padding-left: 20px;
+}
+
+/* Style the footer */
+.footer {
+    background-color: #f1f1f1;
+    padding: 10px;
+    position: fixed;
+    bottom: 0;
+    left: 200px;
+    height: 45px;
+    width: 100%;
+}
+</style>
 </head>
-<frameset>
-    <frame>
-    <frame>
-    <noframes>
-    <body>
-    <p>This page uses frames. The current browser you are using does not support frames.</p>
-    <?php
-echo "hello word"
-	?>
-    </body>
-    </noframes>
-</frameset>
+<body>
+
+<div class="topnav">
+  <a href="#">Link</a>
+  <a href="#">Link</a>
+  <a href="#">Link</a>
+</div>
+
+<div class="sidenav">
+  <a href="#">Link</a>
+  <a href="#">Link</a>
+  <a href="#">Link</a>
+</div>
+
+<div class="content">
+  <h2>CSS Template</h2>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+  <p>A full-height, fixed sidenav and content.</p>
+</div>
+
+<div class="footer">
+  <p>Footer</p>
+</div>
+
+</body>
 </html>
