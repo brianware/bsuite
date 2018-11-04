6b983fe3696b21cd716e90adac1592036c07df4f
Brian <brian@brianware.com>
2018-11-04 10:01:57 -0800

    My First COmmit

--- home.php
+++ home.php
@@ -19,20 +19,41 @@ if (login_check($mysqli) == false) {
 <body>
 <div class="topnav">
  <p style="font-weight:bold;color:white;padding:0px 10px;float: left;">B Suite 2018</p>
- <a style="margin-left:85px;"href="#">Link</a>
- <a href="#">Link</a>
- <a href="#">Link</a>
+ <a style="margin-left:5px;"href="#">Sales</a>
+ <a href="#">Inventory</a>
+ <a href="#">Report</a>
   <div class="topnav-right">
    <a style="font-weight:bold;color:white;padding-left:10px;text-align:right;" href="includes/logout.php">Logout</a>
   </div>
 </div>
 
 <div class="content">
- <p>Welcome <?php echo htmlentities($_SESSION['username']); ?>!</p>
+ <h1>Welcome <?php echo htmlentities($_SESSION['username']); ?>!</h1>
+ <p>Inventory List - Enter barcode to check stock balance</p>
+
+<form action="/action_page.php">
+  Item Number<br>
+  <input type="text" name="firstname" value="95812311231">
+  <br>
+  <input type="submit" value="Submit">
+</form> 
+
+<br>
+
+ <table style="width:100%">
+  <tr>
+    <th>Item No</th>
+    <th>Quantity</th>
+  </tr>
+  <tr>
+    <td>95812311231</td>
+    <td>3</td>
+</table> 
+
 </div>
 
 <div class="sidenav">
- <a href="#">Link</a>
+ <a href="#">Invoice</a>
 </div>
 
 <div class="footer">
