140ca2499d3463abf9d02603a7adab9a5c155762
Brian <brian@brianware.com>
2018-11-06 00:08:53 +0800

    My First Commit

--- index.php
+++ index.php
@@ -16,44 +16,94 @@ if (login_check($mysqli) == true) {
 <title>Index</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="styles/style.css">
 <script type="text/JavaScript" src="js/sha512.js"></script> 
 <script type="text/JavaScript" src="js/forms.js"></script> 
-<script type="text/JavaScript" src="js/style.js"></script>
 
-<style type="text/css">
-    .fieldset-auto-width {
-         display: inline-block;
-    }
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+ /* Bordered form */
+form {
+    border: 3px solid #f1f1f1;
+}
+
+/* Full-width inputs */
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+/* Set a style for all buttons */
+.button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+  }
+
+/* Add a hover effect for buttons */
+.button:hover {
+    opacity: 0.8;
+}
+
+/* Center the image and position the close button */
+.imgcontainer {
+    text-align: center;
+    margin: 24px 0 12px 0;
+    position: relative;
+}
+
+img.avatar {
+    width: 40%;
+    border-radius: 50%;
+}
+
+/* Add padding to containers */
+.container {
+    padding: 16px;
+}
+
+/* Modal Content/Box */
+.modal-content {
+    
+    background-color: #fefefe;
+    border: 1px solid #888;
+    margin: 50px auto;
+    display: table;
+    width: 400px;
+}
 </style>
+
 </head>
-
 <body>
-<div class="topnav" id="topnav">
- <p style="font-weight:bold;color:white;padding:0px 10px;float:left;">B Suite 2018</p>
-</div>
 
-<div class="content grid-container">
- <h1>Welcome to B Suite</h1>
 <?php
         if (isset($_GET['error'])) {
             echo '<p class="error">Error Logging In!</p>';
         }
 ?> 
- <form action="includes/process_login.php" method="post" name="login_form"> 	
-  <fieldset class="fieldset-auto-width">
-   <legend>Login</legend>
-   <label for='username' >User Name:</label>
-   <input type='text' name='username' />
-   <label for='password' >Password:</label>
-   <input type='password' name='password' id='password'/>
-   <input type='button' class="button" name='Submit' value='Login' onclick="formhash(this.form, this.form.password);"/>
-  </fieldset>		
- </form>
-</div>
+<form class="modal-content" action="includes/process_login.php" method="post" name="login_form">
+ <div class="imgcontainer">
+  <img src="avatar.png" class="avatar">
+ </div>
+    
+ <div class="container">
+  <label for='username'><b>Username</b></label>
+   <input type="text" placeholder="Enter Username" name='username' required>
+  <label for='password'><b>Password</b></label>
+   <input type='password' placeholder="Enter Password" name='password' id='password' required>
+ </div>
 
-<div class="footer">
- <p>Copyright © 2018 by Brianware Inc</p>
-</div>
+ <div class="container" style="background-color:#f1f1f1">
+  <input type='button' class="button" name='Submit' value='Login' onclick="formhash(this.form, this.form.password);"/>
+  <p style="text-align:right;font-size:10px;">Copyright © 2018 by Brianware Inc</p>
+ </div>
+</form>
 </body>
 </html>
